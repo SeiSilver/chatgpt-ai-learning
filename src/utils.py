7921d1c31019287818208be35362d21580f8aed7
Dat Pham <dpham@paramountcommerce.com>
2023-12-19 16:19:51 +0700

    update GPT model

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 client = openai.OpenAI()
 
 
-def get_completion(prompt_input, model="gpt-3.5-turbo"):
+def get_completion(prompt_input, model="gpt-3.5-turbo-1106"):
     messages = [{"role": "user", "content": prompt_input}]
     data = client.chat.completions.create(
         model=model,
