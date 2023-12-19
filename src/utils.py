d54c8e3bf07336c7607a00061d64e4ec55243f2a
Dat Pham <dpham@paramountcommerce.com>
2023-12-19 15:42:02 +0700

    add chap 1

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+
+import os
+
+import openai
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+client = openai.OpenAI()
+
+
+def get_completion(prompt_input, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt_input}]
+    data = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return data.choices[0].message.content
